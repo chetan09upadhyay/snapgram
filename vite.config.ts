a8edf00770202851cbe9eb02e745d10720df43e5
chetan09upadhyay <chetanupadhyay653@gmail.com>
2023-11-26 09:37:50 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,4 @@ export default defineConfig({
     },
   },
 })
-
-// import path from 'path'
-// import react from "@vitejs/plugin-react"
-// export default {
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'),
-//     },
-//   },
-// };
+ 
