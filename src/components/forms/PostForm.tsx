da180d446c9636a709830fe94ab8c3c0f8c05f70
chetan09upadhyay <chetanupadhyay653@gmail.com>
2023-11-26 10:08:00 +0530

    Second commit

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -13,7 +13,6 @@ import { useUserContext } from "@/context/AuthContext"
 import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 import { useToast } from "../ui/use-toast"
 import { useNavigate } from "react-router-dom"
-import {  updatePost } from "@/lib/appwrite/api"
   
 type PostFormProps ={
     post?: Models.Document;
